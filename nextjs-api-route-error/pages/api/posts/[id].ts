e9e6640a83f5660f0c772d5f4c61adb5c2bed800
olaven <olav@sundfoer.com>
2020-06-01 12:27:34 +0200

    added expaining comment in [id].ts

--- nextjs-api-route-error/pages/api/posts/[id].ts
+++ nextjs-api-route-error/pages/api/posts/[id].ts
@@ -1,5 +1,10 @@
 export default function getPost(request, response) {
 
+    /*
+        NOTE: 
+        From browser: id is defined (index.tsx)
+        From test: id is undefined (post.test.ts)
+    */
     const { query: { id } } = request;
 
     response
